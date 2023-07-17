c61147553c601a25ff546aa03465327acc7e581d
pjil127 <jillpantigcs@gmail.com>
2023-07-16 23:02:50 -0700

    Fixing bexier curve code

--- Bezier_Curve.h
+++ Bezier_Curve.h
@@ -14,9 +14,9 @@
 // Calculates the midpoint of start and end points!
 Coordinates generate_control_point(Coordinates c0, Coordinates c2) {
     Coordinates c1;
-    c1.x = (c1.x + c1.x) / 2;
-    c1.y = (c1.y + c1.y) / 2;
-    c1.z = (c1.z + c1.z) / 2;
+    c1.x = (c0.x + c2.x) / 2;
+    c1.y = (c0.y + c2.y) / 2;
+    c1.z = (c0.z + c2.z) / 2;
     return c1;
 }
 
@@ -24,10 +24,6 @@ Coordinates generate_control_point(Coordinates c0, Coordinates c2) {
 // c0 = start point, c1 = control point, c2 = end point
 // t = time interval
 Coordinates calculate_bezier_points(float t, Coordinates c0, Coordinates c1, Coordinates c2) {
-    float u = 1 - t;
-    float tt = t * t;
-    float uu = u * u;
-    
     Coordinates points;
     points.x = pow((1 - t), 2) * c0.x + 2 * (1 - t) * t * c1.x + (t * t) * c2.x;
     points.y = pow((1 - t), 2) * c0.y + 2 * (1 - t) * t * c1.y + (t * t) * c2.y;
